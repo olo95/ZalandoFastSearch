ac46449899f45709720e9f3a204b900ef7c3bc9d
Alexander Stolar <alexander.stolar@yahoo.com>
2018-07-10 02:06:20 +0200

    WIP

---
+++ zalando_fast_search/lib/Preset.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Preset {
+  Preset(this.title);
+  final String title;
+}
---
+++ zalando_fast_search/lib/PresetDataTable.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class PresetDataTable extends StatefulWidget {
+  @override
+  PresetDataTableState createState() => new PresetDataTableState();
+}
+
+class PresetDataTableState extends State<PresetDataTable> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
